a960ffbaa22e42c2f749971cec93b71a9a1e2aca
sha-net <shavit.netzer@gmail.com>
2013-11-11 14:27:17 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShaNet::Application.config.secret_key_base = 'e0b3a83fd9eb747c2e58919b4ecdf01a4a0136ff87c5350ffd7e01cee145ab8e2704923541636bb87479640963e71377a8d73868f0a68c139fe56e57c231e868'
